8bbb448504c593c5e84d8501fea84a42e883cc04
dinhtanhuy17 <2051010099huy@ou.edu.vn>
2024-08-22 10:07:22 +0700

    arrow function

---
+++ main.js
@@ -0,0 +1,12 @@
+// const logger = function(log) {
+//     console.log(log) ; 
+//    }
+//    logger('Massage');
+
+   // chuyển thành arrow function 
+
+   const logger = (log) => {
+    console.log(log) ; 
+   }
+   logger('Massage');
+ 
