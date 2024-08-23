5948a504b0393075cd0f840f54bd8537ae6491fa
dinhtanhuy360 <dinhtanhuy.360@gmail.com>
2024-08-23 22:02:28 +0700

    arrow fuction

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
-// const logger = function(log) {
-//     console.log(log) ; 
-//    }
-//    logger('Massage');
 
-   // chuyển thành arrow function 
 
-   const logger = (log) => {
-    console.log(log) ; 
-   }
-   logger('Massage');
-  var a = 1 + 2; 
-  var d = 1 + 2; 
-  var c = 1 + 2; 
-   var f = 1 + 2; 
-  var c = 1 + 2; 
-  var comflect = 1 + 2; 
-  var a = 'hello world';
-  var p = 'hello world';
-  var c = 'hello world';  
-// thay doi cua huy360
-var c = 'hello world';  
+
+//  const logger = function logger(log) {
+//   console.log(log);
+
+//  }
+//  logger('Message');
+
+ // chuyển thành arrow function 
+ const logger =  (log) => {
+  console.log(log);
+
+ }
+ logger('Message');
+
+
+ // hàm sum arrow function 
+
+ const sum = (a,b) => {
+
+  return a + b;
+ }
+ console.log(sum(2,3));
+
+// đối tượng 
+const course = {
+  name: 'javascript ',
+  getName : function () {
+      return this.name;
+  }
+};
+console.log(course.getName());
+
+// arrow function sử dụng để làm contructor sẽ báo lỗi
+const mon = function(name,price) {
+  this.name = name;
+  this.price = price;
+}
+const jsCourse = new course('Javvascript',10000);
+
+console.log(jsCourse);
+
+// Bài tập
+// function sumEvenNumbers(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] % 2 === 0) {
+//           sum += arr[i];
+//       }
+//   }
+//   return sum;
+// }
+
+const sumEvenNumbers = arr => arr.reduce((sum, num) => num % 2 === 0 ? sum + num : sum, 0);
